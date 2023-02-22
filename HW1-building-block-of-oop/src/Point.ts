12c6724e0370a21238e014b46a054d0e97d55c69
Vu Nguyen <vu_nguyen@epam.com>
2023-02-22 22:43:39 +0700

    add: hw1-building-blocks-of-oop-P1

---
+++ HW1-building-block-of-oop/src/Point.ts
@@ -0,0 +1,35 @@
+export class Point {
+  private x: number;
+  private y: number;
+
+  private getDistance(x: number, y: number): number {
+    return Math.sqrt(Math.pow(x - this.x, 2) + Math.pow(y - this.y, 2));
+  }
+
+  //TypeScript allows you to declare overloads but you can only have one implementation and that implementation must have a signature that is compatible with all overloads.
+  constructor();
+  constructor(x: number, y: number);
+  constructor(x?: number, y?: number) {
+    this.x = x ?? 0;
+    this.y = y ?? 0;
+  }
+
+  public toString(): string {
+    return `(${this.x}, ${this.y})`;
+  }
+
+  public distance(): number;
+  public distance(point: Point): number;
+  public distance(x: number, y: number): number;
+  public distance(pointOrX?: Point | number, y?: number): number {
+    if (pointOrX instanceof Point) {
+      return this.getDistance(pointOrX.x, pointOrX.y);
+    }
+
+    if (!!pointOrX && !!y) {
+      return this.getDistance(pointOrX, y);
+    }
+
+    return this.getDistance(0, 0);
+  }
+}
